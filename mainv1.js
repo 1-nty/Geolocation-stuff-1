3db2b8b5f40940b4c2f39560a02d055742b22dc6
1-nty <46821853+1-nty@users.noreply.github.com>
2019-02-16 12:55:20 -0500

    Create mainv1.js

---
+++ mainv1.js
@@ -0,0 +1,29 @@
+// check if available
+if (navigator.geolocation) {
+  // 🗺️ yep, we can proceed!
+  console.log('yep')
+} else {
+  // no can do
+  console.log('no')
+}
+
+//design an example sucess Function
+if (navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(displayLocationInfo);
+}
+
+
+function displayLocationInfo(position){
+  const pc = position.coords;
+  const lng = pc.longitude;
+  const lat = pc.latitude;
+  const accuracy = pc.accuracy;
+  const altitude = pc.altitude;
+  const altitudeAccuracy = pc.altitudeAccuracy;
+  const heading = pc.heading;
+  const speed = pc.speed;
+  // timestamp
+  const time = position.timestamp;
+
+  console.log(`{longitude: ${ lng }, latitude: ${ lat }}`);
+}
